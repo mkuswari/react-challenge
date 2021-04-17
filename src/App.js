ee1ba63a48cc2f480bc3da00d1c21b07eb6c993f
Muhammad Kuswari <muh.kuswari10@gmail.com>
2021-04-17 17:13:17 +0800

    react challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import icon from './icon.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <img src={icon} alt="App Icon"></img>
+        <h1 className="VerificationText">Verifikasi Diri Anda</h1>
+        <form className="OtpForm" method="POST">
+          <input type="text" className="OtpInput" maxLength="1"></input>
+          <input type="text" className="OtpInput" maxLength="1"></input>
+          <input type="text" className="OtpInput" maxLength="1"></input>
+          <input type="text" className="OtpInput" maxLength="1"></input>
+          <input type="text" className="OtpInput" maxLength="1"></input>
+          <input type="text" className="OtpInput" maxLength="1"></input>
+        </form>
+        <p className="description">Kami telah mengirimkan kode verifikasi OTP ke nomer Whatsapp yang telah
+          <br></br> anda daftarkan saat proses pendaftaran</p>
+        <p className="CountDownTimer">04 : 59</p>
+        <button type="submit" className="VerificationButton">Verifikasi</button>
+        <a href="#" className="RequestVerificationCode">Kirim Ulang Kode</a>
+      </div>
     </div>
   );
 }
+
 
 export default App;
