265f4c2df8c6336746e2d299f4c3edef47b63fd3
nersus15 <fathur.ashter15@gmail.com>
2021-10-03 20:19:39 +0800

    menambahkan countdown untuk resend OTP selama 5 menit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import icon from './icon.svg';
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import OtpInput from './components/OtpInput'
 import OtpAction from './components/OtpAction'
 
 function App() {
+  useEffect(CountDownTimer)
   return (
     <div className="App">
       <div className="content">
         <img src={icon} alt="App Icon"></img>
         <OtpInput />
-        <p className="CountDownTimer">04 : 59</p>
+        <p id="timer" className="CountDownTimer"></p>
         <OtpAction />
       </div>
     </div>
   );
 }
 
+const CountDownTimer = () => {
+  const timer = document.getElementById("timer");
+  const resend = document.getElementById('resend');
+  let batas = 300000;
+
+  resend.style.display = 'none';
+  const interval = setInterval(
+    () => {
+      batas = batas - 1000;
+      let menit = Math.floor((batas) / 60000);
+      let detik = (((batas) % 60000) / 1000).toFixed(0);
+      timer.innerHTML = menit + ':' + detik;
+    }, 1000
+  );
+
+  setTimeout(
+    () => {
+      resend.style.display = 'block';
+      clearInterval(interval);
+    }, 300001
+  );
+}
 
 export default App;
